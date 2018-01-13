8fbe0cf6aa5fa854ab51ae4d61ac91b74004c77c
shinesuko <kozotakeuchi0123@gmail.com>
2018-01-14 00:31:38 +0900

    edit

--- pydcjs/main.py
+++ pydcjs/main.py
@@ -197,10 +197,7 @@ def lineChart(figure=1,make_fig=False,width=200,height=200,dim='',group='Count'\
 	else:
 		# gp="""dim.group(function (d) {
 		# return d."""+str(group)+""";})"""
-		gp="""dim.group().reduce(
-		function(p, v) {return v.{GROUP};},
-    	function(p, v) {return v.{GROUP};},
-    	function() {return {};});""".format(GROUP=group)
+		gp="""dim.group().reduce(function(p, v) {return v.{GROUP};},function(p, v) {return v.{GROUP};},function() {return {};});""".format(GROUP=group)
 	print gp
 	chart="""
 	d3.select("#chart_{figure}").append("p").text("lineCart: {dim}");
