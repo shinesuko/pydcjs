c07f92bc9e7544fae07d542cbe1f49873272d02d
shinesuko <kozotakeuchi0123@gmail.com>
2018-01-13 22:15:03 +0900

    bug fix

--- pydcjs/main.py
+++ pydcjs/main.py
@@ -194,9 +194,8 @@ def lineChart(figure=1,make_fig=False,width=200,height=200,dim='',group='Count'\
 		display(HTML(html))
 	if group=='Count':
 		gp="""dim.group().reduceCount()"""
-		print 'count'
 	else:
-		gp="""dim.group().reduceSum(function (d) {
+		gp="""dim.group(function (d) {
 		return d."""+str(group)+""";})"""
 	chart="""
 	d3.select("#chart_{figure}").append("p").text("lineCart: {dim}");
